ce327cb0c0b5da06cb094e9e71ea08e0ea5a5678
Yeskina <angelina.yeskina@gmail.com>
2020-01-02 22:20:52 +0200

    Add travisci and code climate

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,8 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync'
+import app from '../index'
 
-console.log('Welcome to the Brain Games!')
-
-const userName = readlineSync.question('May I have your name? ')
-
-console.log(`Hi ${userName}!`)
+app()
--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-import userName from './bin';
-export default userName;
+import readlineSync from 'readline-sync'
+
+export default () => {
+  console.log('Welcome to the Brain Games!')
+
+  const userName = readlineSync.question('May I have your name? ')
+
+  console.log(`Hi ${userName}!`)
+}
